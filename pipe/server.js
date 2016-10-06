af4fe700ff0b2ea7e730a1629662b131d4f06bc2
Albert Galiullov <simbirsoft_a.galiullov@abbyy-ls.com>
2016-10-06 22:36:24 +0400

    Proxy data in pipe by _read and _write

--- pipe/server.js
+++ pipe/server.js
@@ -1,5 +1,31 @@
 'use strict';
 var net = require('net');
+var stream = require('stream');
+
+class myStream extends stream.Duplex {
+    constructor(options) {
+        super(options);
+        this.buffer = new Buffer(0);
+        this.marker = options.marker;
+    }
+
+    _read(size) {
+        var buf = this.buffer.slice(0, size);
+        this.buffer = this.buffer.slice(size);
+
+        if (buf.length) {
+            this.push(buf);
+        }
+    }
+
+    _write(buf, encoding, cb) {
+        buf = this.marker + buf.toString().replace(/\n/g, '\n' + this.marker);
+        buf = new Buffer(buf);
+        this.buffer = Buffer.concat([this.buffer, buf]);
+        this._read(buf.length);
+        cb();
+    }
+}
 
 var server = net.createServer((connection) => {
     console.log('Connect');
@@ -17,8 +43,15 @@ var server = net.createServer((connection) => {
         }
     });
 
-    connection.pipe(process.stdout);
-    process.stdin.pipe(connection);
+    var o = new myStream({marker: '>'});
+    var i = new myStream({marker: '<'});
+
+
+    connection.pipe(i).pipe(process.stdout);
+    process.stdin.pipe(o).pipe(connection);
+
+    //connection.pipe(process.stdout);
+    //process.stdin.pipe(connection);
 });
 
 server.listen(3030, () => {
