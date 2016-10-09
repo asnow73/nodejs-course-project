2ad7611949382dd192e467a62c88655c39e16fb7
Albert Galiullov <simbirsoft_a.galiullov@abbyy-ls.com>
2016-10-09 19:39:41 +0400

    Create cluster of workers

--- app.js
+++ app.js
@@ -1,39 +1,59 @@
 'use strict';
 
-const express = require('express');
-const cors = require('cors');
-const bodyparser = require('body-parser');
-const config = require('./config'); // get our config file
-const Mongodb = require('./models/Mongodb');
-const authenticate = require('authenticate-package');
-const users = require('./models/user');
-const app = express();
-require('./websocket/server');
+var cluster = require('cluster');
 
-app.set('port', config.port);
-authenticate.init(config.secret, users);
+if (cluster.isMaster) {
 
-app.use(cors());
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: false }));
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
 
-app.use('/', require('./controllers/index'));
-app.use('/chat', require('./controllers/chat'));
-app.use('/register', require('./controllers/register'));
-app.use(authenticate);
-app.use('/user', require('./controllers/user'));
-app.use('/event', require('./controllers/event'));
-app.use('/quiz', require('./controllers/quiz'));
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  return res.status(500).send(err);
-});
-
-let startServer = () => {
-	app.listen(app.get('port'), () => {
-        console.log('Express server listening on port', app.get('port'));
+    cluster.on('exit', function (worker) {
+        console.log('Worker %d died :(', worker.id);
+        cluster.fork();
     });
-};
 
-Mongodb.connect(startServer);
+
+} else {
+    const express = require('express');
+    const cors = require('cors');
+    const bodyparser = require('body-parser');
+    const config = require('./config'); // get our config file
+    const Mongodb = require('./models/Mongodb');
+    const authenticate = require('authenticate-package');
+    const users = require('./models/user');
+    const app = express();
+    require('./websocket/server');
+
+    app.set('port', config.port);
+    authenticate.init(config.secret, users);
+
+    app.use(cors());
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: false }));
+
+    app.use('/', require('./controllers/index'));
+    app.use('/chat', require('./controllers/chat'));
+    app.use('/register', require('./controllers/register'));
+    app.use(authenticate);
+    app.use('/user', require('./controllers/user'));
+    app.use('/event', require('./controllers/event'));
+    app.use('/quiz', require('./controllers/quiz'));
+
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        return res.status(500).send(err);
+    });
+
+    let startServer = () => {
+        app.listen(app.get('port'), () => {
+            console.log('Express server listening on port', app.get('port'));
+        });
+    };
+
+    Mongodb.connect(startServer);
+}
